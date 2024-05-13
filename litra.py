c6603af7689d4fd0ef2026a22f2ab691b97117ff
partha235 <parthaa235@gmail.com>
2024-05-13 22:24:18 +0530

    reflector over

--- litra.py
+++ litra.py
@@ -38,4 +38,86 @@ def refractor(size):
     t.fd(20)
 
 refractor(10)
+
+def reflector():
+    # light entering 
+    t.speed(1) # pen speed.
+    t.setup(width=1000, height=600) # declaring windows size
+    print(t.pos()) # pen position
+    t.pencolor("red")
+    t.penup()
+    t.setpos(-250,0) 
+    sleep(2)
+
+    # light entering
+    t.pendown()
+    t.pensize(100)
+    t.fd(50)
+    t.penup()
+    t.fd(5+5+10+30+2)
+    t.pencolor("black")
+    t.pendown()
+    t.pensize(20)
+    t.fd(5)
+
+
+
+    # light ray in side of secondary mirror
+    l1=t.Turtle()
+    l2=t.Turtle()
+    l1.penup()
+    l2.penup()
+    l1.setpos(-158,30)
+    l2.setpos(-158,-30)
+    l1.pendown()
+    l2.pendown()
+    l1.setheading(0)
+    l2.setheading(0)
+    l1.bk(30)
+    l2.bk(30)
+    l1.pensize(35)
+    l1.pencolor("red")
+    l2.pensize(35)
+    l2.pencolor("red")
+    l1.fd(230)
+    l2.fd(230)
+
+    # drawing reflective mirror
+    t.penup()
+    t.bk(5)
+    t.fd(200)
+    t.pendown()
+    t.fillcolor("blue")
+    t.begin_fill()
+    t.pensize(1)
+    t.setheading(270)
+    t.fd(50)
+    t.left(90)
+    t.fd(10)
+    t.left(90)
+    t.fd(100)
+    t.left(90)
+    t.fd(10)
+    t.left(90)
+    t.fd(50)
+    t.end_fill()
+
+
+    # reflecting light
+    l1.pencolor("blue")
+    l1.pensize(5)
+    l1.right(170)
+    l1.fd(178)
+    l1.setheading(270)
+    l1.fd(100)
+
+
+    l2.pencolor("blue")
+    l2.pensize(5)
+    l2.left(170)
+    l2.fd(178)
+    l2.setheading(270)
+    l2.fd(100) 
+
+# reflector()
 t.mainloop()
--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import turtle as t  # import library
 from time import sleep
-# screen=t.Screen()
-# print(screen)
-# screen._root.geometry("+200+200")
+
+
+
+# light entering 
 t.speed(1) # pen speed.
 t.setup(width=1000, height=600) # declaring windows size
 print(t.pos()) # pen position
 t.pencolor("red")
 t.penup()
 t.setpos(-250,0) 
+sleep(2)
+
+# light entering
 t.pendown()
 t.pensize(100)
 t.fd(50)
@@ -18,36 +22,72 @@ t.pencolor("black")
 t.pendown()
 t.pensize(20)
 t.fd(5)
-t.penup()
-t.bk(5)
+
+
+
+# light ray in side of secondary mirror
 l1=t.Turtle()
 l2=t.Turtle()
 l1.penup()
 l2.penup()
-l1.setpos(-158,20)
-l2.setpos(-158,-20)
+l1.setpos(-158,30)
+l2.setpos(-158,-30)
 l1.pendown()
 l2.pendown()
 l1.setheading(0)
 l2.setheading(0)
-l1.pensize(30)
+l1.bk(30)
+l2.bk(30)
+l1.pensize(35)
 l1.pencolor("red")
-l2.pensize(30)
+l2.pensize(35)
 l2.pencolor("red")
-l1.fd(200)
+l1.fd(230)
+l2.fd(230)
+
+# drawing reflective mirror
+t.penup()
+t.bk(5)
+t.fd(200)
+t.pendown()
+t.fillcolor("blue")
+t.begin_fill()
+t.pensize(1)
+t.setheading(270)
+# t.fd(50)
+# t.left(90)
+# t.fd(10)
+# t.left(90)
+# t.fd(100)
+# t.left(90)
+# t.fd(10)
+# t.left(90)
+# t.fd(50)
+t.circle(250,10)
+t.setheading(0)
+
+
+t.end_fill()
+
+
+
+
+
+# reflecting light
 l1.pencolor("blue")
 l1.pensize(5)
-l1.right(90+45+10+10+10+10)
-l1.fd(175)
-l1.setheading(270)
-l1.fd(100)
+l1.right(170)
+l1.fd(178)
+l1.setheading(0)
+l1.fd(250)
 
-l2.fd(200)
+
+
 l2.pencolor("blue")
 l2.pensize(5)
-l2.left(175)
-l2.fd(175)
-l2.setheading(270)
-l2.fd(100)
+l2.left(170)
+l2.fd(178)
+l2.setheading(0)
+l2.fd(250)
 
 t.mainloop()
